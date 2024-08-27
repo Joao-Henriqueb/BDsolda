e3f0fad802360040f26b803b74b1e69536edeac7
João Henrique <joaohenriquerbueno@gmail.com>
2024-08-27 11:40:19 -0300

    subindo arquivos test 3

--- index.js
+++ index.js
@@ -62,17 +62,13 @@ app.get('/artigos', (req, res) => {
         ...doc.data(),
         uid: doc.id,
       }));
-
+      /*
       artigos.map((artigos) => {
         introArtigos.push(artigos.intro);
       });
-      console.log('intro');
       console.log(introArtigos);
-      console.log('2');
-      console.log(artigos);
-
-      res.json(introArtigos);
-      //res.render('artigos', { posts: artigos[0] });
+      */
+      res.render('artigos', { posts: artigos[0] });
     });
 });
 

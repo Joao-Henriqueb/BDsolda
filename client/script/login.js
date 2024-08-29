a61806c98e16328c47dc7466b1d17b87596c7acb
João Henrique <joaohenriquerbueno@gmail.com>
2024-08-29 13:59:28 -0300

    arrumando rotas do fetch

--- client/script/login.js
+++ client/script/login.js
@@ -8,13 +8,13 @@ async function login() {
   const email = loginForm.elements['email'].value;
   const password = loginForm.elements['password'].value;
   try {
-    const res = await fetch('http://localhost:5000/login', {
+    const res = await fetch('https://b-dsolda.vercel.app/login', {
       headers: { 'Content-type': 'application/json;charset=UTF-8' },
       method: 'POST',
       body: JSON.stringify({ email, password }),
     });
     if (res.ok) {
-      window.location.replace('http://localhost:5000/novopost');
+      window.location.replace('https://b-dsolda.vercel.app/novopost');
     } else {
       aviso.classList.add('ativo');
       console.log('Email ou senha incorreta');

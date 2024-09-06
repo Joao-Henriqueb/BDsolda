08140be75bca33c69b4a84b3bf5a84fab30d6e5d
João Henrique <joaohenriquerbueno@gmail.com>
2024-09-06 19:19:33 -0300

    testando funcionamento 5

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -5,7 +5,7 @@ const storage = multer.memoryStorage();
 const upload = multer({ storage: storage }); // Configurando multer para armazenar img  na memória
 const { getAuth, signInWithEmailAndPassword } = require('firebase/auth');
 const authController = require('../controllers/authController');
-const { bucket, db } = require('../index');
+//const { bucket, db } = require('../index');
 const { v4: uuidv4 } = require('uuid'); // Para gerar nomes únicos para os arquivos
 
 router.post('/login', async (req, res) => {
@@ -48,7 +48,7 @@ function substituirSrc(linksImgs, html, imgIntro) {
 
   return html;
 }
-/*
+
 router.post(
   '/upload',
   authController.authenticateToken,
@@ -122,5 +122,5 @@ router.post(
     }
   },
 );
-*/
+
 module.exports = router;
